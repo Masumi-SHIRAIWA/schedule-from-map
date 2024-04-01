f662bb61a097af31e40a57ccc802b98e9c9934c8
shiraiwa masumi <masu3_4raiwa@i.softbank.jp>
2024-04-02 00:20:38 +0900

    introduced react-frow and lined up items with grid

---
+++ src/components/MindMap.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useCallback } from "react";
+import ReactFlow, {
+    MiniMap,
+    Controls,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+  } from "reactflow";
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Mind Map" },
+    position: { x: 0, y: 0 },
+  },
+];
+
+const initialEdges = [];
+
+// onLoad関数：ReactFlowにプロパティとして渡す
+const onLoad = (reactFlowInstance) => {
+// fitView: すべてのノードとエッジが表示されるようにビューを調整する
+  reactFlowInstance.fitView();
+};
+
+export default function MindMap() {
+    // 独自のステート管理
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [name, setName] = useState("");
+
+  const addNode = () => {
+    setNodes((e) =>
+      e.concat({
+        id: (e.length + 1).toString(),
+        data: { label: `${name}` },
+        position: {
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+        },
+      })
+    );
+  };
+
+    // コールバックとして定義し無駄なレンダリングを防ぐ
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div id="container" className="flex flex-col w-full h-full ">
+      <div className="flex justify-center">
+          <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          name="title"
+          className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+          />
+          <button
+          type="button"
+          onClick={addNode}
+          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+          >
+          Add Node
+          </button>
+      </div>
+        <ReactFlow 
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onLoad={onLoad}
+            className="flex-grow h-100 w-100">
+
+            <Controls />
+            <MiniMap
+            nodeColor={(n) => {
+                if (n.type === "input") return "blue";
+
+                return "#FFCC00";
+            }}
+            />
+        </ReactFlow>
+    </div>
+
+  );
+}
--- src/pages/projects/[id].jsx
+++ src/pages/projects/[id].jsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import prisma from "@/util/prisma";
 import Image from 'next/image'
 
+import MindMap from '@/components/MindMap'
+
 // SSRの場合
 export async function getServerSideProps(context){
     const { id } = context.params;
@@ -25,15 +27,28 @@ const Project = (props) => {
     console.log(props.data)
 
     return (
-        <div className={`min-h-screen`}>
-                <div className="flex items-center px-10 w-full "> 
+
+        <div className={`grid grid-rows-5 gap-3 w-screen h-screen`}>
+            <div className="flex items-center px-10 w-full "> 
                             <div className="flex-shrink-0">
                     <Image className="w-10 h-10 rounded-full" src="/favicon.ico" width="0" height="0"/>
                 </div>
                 <div className="inline-block font-bold text-2xl leading-10 mt-8 mb-8">Project: {props.data.name}</div>
             </div>
-            
+            <div className="row-span-4">
+                <MindMap/>
+            </div>
         </div>
+
+        // <div className={`flex flex-col min-h-screen`}>
+        //     <div className="flex items-center px-10 w-full "> 
+        //                     <div className="flex-shrink-0">
+        //             <Image className="w-10 h-10 rounded-full" src="/favicon.ico" width="0" height="0"/>
+        //         </div>
+        //         <div className="inline-block font-bold text-2xl leading-10 mt-8 mb-8">Project: {props.data.name}</div>
+        //     </div>
+        //     <MindMap />
+        // </div>
     )
     
 };
